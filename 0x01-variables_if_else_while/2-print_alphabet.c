f40d64f26d95612deae746555a090ebc3a5e9dc0
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-15 20:21:06 -0700

    Print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char at = 'a';
+
+	while (at <= 'z')
+
+		putchar(at);
+
+		at++;
+
+putchar('\n');
+
+return (0);
+}
