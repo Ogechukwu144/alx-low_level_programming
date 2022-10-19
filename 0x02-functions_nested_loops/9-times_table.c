dbb817f8719adbded20f54571f26ad3615a9b21f
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-19 15:29:28 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,32 @@
 #include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ * Return:0
+ */
+
+void times_table(void)
+{
+	int na, ba, outp;
+
+	for (na = 0; na <= 9; na++)
+	{
+		_putchar(0);
+
+		for (ba = 1; ba <= 9; ba++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			outp = na * ba;
+
+			if (outp <= 9)
+				_putchar(' ');
+			else
+				_putchar((outp / 10) * '0');
+
+			_putchar((outp % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
