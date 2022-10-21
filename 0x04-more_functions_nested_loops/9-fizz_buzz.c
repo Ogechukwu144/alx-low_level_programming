ab7b1b65d4dddc98842efe2489f4aa8792e7be49
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-20 20:14:00 -0700

    Print the numbers 1 to 100, FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers 1 to 100: Fizzbuzz
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 1; j <= 100; j++)
+	{
+		if (j % 3 == 0 && j % 5 != 0)
+		{
+			printf("Fizz");
+		} else if (j % 5 == 0 && j % 3 != 0)
+		{
+			printf("Buzz");
+		} else if (j % 3 == 0 && j % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if (j == 1)
+		{
+			printf("%d", j);
+		} else
+		{
+			printf(" %d", j);
+		}
+	}
+	printf("\n");
+	return (0);
+}
