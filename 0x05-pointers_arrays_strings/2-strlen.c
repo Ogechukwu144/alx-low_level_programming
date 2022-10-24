e1e8e174487bef3f0d58914b60ddc4ee9dad76f5
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-24 12:46:37 -0700

    Task 2 length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string in question
+ *
+ * Return:0
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s[length] != '\0')
+	{
+		length++;
+	}
+		return (length);
+}
