04b4830d3911d54ac1442ea1539f8743cbf00992
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-24 11:11:04 -0700

    Task 1, n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int, and updates to 98
+ * @n: int parameter
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n);
+{
+	int *n;
+
+	*n = 98
+}
