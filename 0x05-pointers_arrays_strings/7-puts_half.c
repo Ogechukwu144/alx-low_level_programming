69575f806db247b6546c43c8cccc25eab071871f
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-24 16:16:32 -0700

    Task 7 print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string parameter
+ *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (1 = 0; str[i] != '\0'; i++)
+
+		if (i % 2 == 0)
+		{
+			for (n = 1 / 2; str[n] != '\0'; n++)
+				_putchar(str[n]);
+		} else
+		{
+			for (n = ((1 - 1) / 2) + 1; str[n] != '\0'; n++)
+				_putchar(str[n]);
+		} _putchar('\n');
+}
