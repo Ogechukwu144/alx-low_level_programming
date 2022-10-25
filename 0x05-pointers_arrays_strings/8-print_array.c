e0c7e42377833a941637a3421b5593a605fb6b07
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-24 17:08:50 -0700

    Task 8 prints n element of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array integer
+ * @n: number of values to be printed
+ *
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int u;
+
+	for (u = 0; u < n; u++)
+	{
+		printf("%d", a[u]);
+
+		if (u != n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
