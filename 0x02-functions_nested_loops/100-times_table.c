d495a00047df70ee9c9259fba5596f0d1f050657
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-26 13:42:47 -0700

    Task 12 Advanced

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the n times table
+ * @n: number times table
+ *
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+
+			for (j = 1; j <= n; j++)
+			{
+				k = i * j;
+
+				_putchar(44);
+				_putchar(32);
+
+				if (k <= 9)
+				{
+					_putchar(32);
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
+				}
+				else
+				{
+					_putchar(((k / 100) % 10) + 48);
+					_putchar(((k / 10) % 10) + 48);
+					_putchar((k % 10) + 48);
+				}
+			}
+
+			_putchar('\n');
+		}
+	}
+}
