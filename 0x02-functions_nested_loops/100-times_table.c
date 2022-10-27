f722718e6fb408cb09ffa13445428515db02e3e7
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-27 01:28:10 -0700

    Task 12 Advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table
- * @n: number times table (0 < n <= 15)
- *
+ * print_times_table - prints the n times table
+ * @n: represents the n times table
  * Return: 0
  */
 
@@ -11,33 +10,41 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar(48);
-
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				k = i * j;
+				k  = i * j;
 
-				_putchar(44);
-				_putchar(32);
-
-				if (k <= 9)
+				if (k < 10 && j > 0)
 				{
-					_putchar(32);
+					_putchar(' ');
+					_putchar(' ');
+				} else if (k >= 10 && j > 0 && k < 100)
+				{
+					_putchar(' ');
+				}
+				if (k < 10)
+				{
+					_putchar((k % 10) + 48);
+				} else if (k >= 10 && k < 100)
+				{
 					_putchar((k / 10) + 48);
 					_putchar((k % 10) + 48);
-				}
-				else
+				} else if (k >= 100)
 				{
-					_putchar(((k / 100) % 10) + 48);
+					_putchar((k / 100) + 48);
 					_putchar(((k / 10) % 10) + 48);
 					_putchar((k % 10) + 48);
 				}
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-
 			_putchar('\n');
 		}
 	}
