5817bf3cd9be24358f59b4d5833d0be91f0848ec
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-10-29 08:44:03 -0700

    Advanced task function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,81 @@
 #include "main.h"
 
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 /**
- * infinite_add - add two integers
- * @n1: 1st number to add
- * @n2: 2nd number to add
- * @r: result
- * @size_r: size of r
- * Return: 0
+ * add_strings - add the numbers
+ * @n1: string
+ * @n2: string
+ * @r: the buffer
+ * @r_index: the index of buffer
+ *
+ * Return: pointer to result
+ */
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+
+	else if (tens && r_index < 0)
+		return (0);
+
+	return (r + r_index + 1);
+}
+
+/**
+ * infinite_add - add two numbers
+ * @n1: 1st number
+ * @n2: 2nd number
+ * @r: buffer to store result
+ * @size_r: the buffer size
+ *
+ * Return: pointer
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, t = 0, q, l = 0, p = 0, n = size_r;
+	int index, n1_len = 0, n2_len = 0;
 
-	while (n1[i])
-	{
-		i++;
-	}
-	while (n2[t])
-	{
-		t++;
-	}
-	i--, t--;
-	size_r = i >= t ? i + 2 : t + 2;
-	r[size_r] = '\0', size_r--, n = n - size_r;
-	while (size_r > 0)
-	{
-		if (i >= 0 && t >= 0)
-		{
-			if ((p == 0 && (n1[i] != '0' || n2[t] != '0')) || p > 0)
-			{
-				q = ((((n1[i] - 48) + (n2[t] - 48)) + l) % 10) + '0';
-			}
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
 
-			l = (((n1[i] - 48) + (n2[t] - 48) + l) / 10);
-		} else if (i >= 0)
-		{
-			q = (n1[i] + l - 48) % 10 + '0', l = (n1[i] + l - 48) / 10;
-		} else if (t >= 0)
-		{
-			q = (n2[t] + l - 48) % 10 + '0', l = (n2[t] + l - 48) / 10;
-		} else
-		{
-			break;
-		}
-		r[size_r] = q, i--, t--, size_r--, p = 1;
-	}
-	r[size_r] = l + '0';
-	return (n <= 1 ? 0 : &r[0]);
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
 }
