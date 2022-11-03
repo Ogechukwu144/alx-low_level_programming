b5786d649173e48ead12b3d57c4fee41bdd37d9c
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-11-03 08:57:11 -0700

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,16 +4,16 @@
  * _puts_recursion - prints a string
  * @s: string
  *
- * Return: no return
+ * Return: no return.
  */
 
 void _puts_recursion(char *s)
 {
-	if  (s != '\0')
+	if  (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-
+	else
 	_putchar('\n');
 }
