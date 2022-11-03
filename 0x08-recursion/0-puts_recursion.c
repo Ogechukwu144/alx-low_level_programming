f98f3e389d988a664d91e3bdc0ed352868776c4a
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-11-03 07:53:57 -0700

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s != '\0') /*if s != null terminator keep going*/
 	{
 		_puts_recursion(s + 1);
 		_putchar(*s);
 	}
 }
 
-int main(void)
 {
 	char s[] = "Puts with recursion";
 
