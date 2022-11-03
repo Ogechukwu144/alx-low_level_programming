c036ab5464596a49be0124706dc0b376f6ad5238
Ogechukwu144 <ogechi.ezeamama@gmail.com>
2022-11-03 14:46:54 -0700

    An empty string is a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * Firstly, we need to check the length of the string
+ * @s: String
+ * Return: length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+
+/**
+ * checkpalindrome - checks each character of the string for palindrome
+ * @s: string to be checked
+ * @i: iterator
+ * @len: length of string
+ * Return: 1 if palindrome, 0 if not.
+ */
+int checkPalindrome(char *s, int i, int len)
+{
+	if (*(s + 1) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (checkPalindrome(s, i + 1, len - 1));
+}
+
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ * Return: 1 if palindrome, 0 if otherwise.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (checkPalindrome(s, 0, _strlen_recursion(s)));
+}
